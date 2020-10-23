98fd03cfce17f61c2a734ebd1d4b7fd30d25a900
FlynnRider-000 <coronarider01@gmail.com>
2020-10-23 23:49:09 +1000

    some issue fix keyboard dismiss on note edit

--- src/pages/delivery_pickup.dart
+++ src/pages/delivery_pickup.dart
@@ -91,179 +91,188 @@ class _DeliveryPickupWidgetState extends StateMVC<DeliveryPickupWidget> {
           new ShoppingCartButtonWidget(iconColor: Theme.of(context).hintColor, labelColor: Theme.of(context).accentColor),
         ],
       ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.symmetric(vertical: 10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(left: 20, right: 10),
-              child: (_con.carts.length > 0 && _con.carts[0].product.market.pay_on_pickup) ? ListTile(
-                contentPadding: EdgeInsets.symmetric(vertical: 0),
-                leading: Icon(
-                  Icons.domain,
-                  color: Theme.of(context).hintColor,
-                ),
-                title: Text(
-                  S.of(context).pickup,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-                subtitle: Text(
-                  S.of(context).pickup_your_product_from_the_market,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: Theme.of(context).textTheme.caption,
-                ),
+      body: GestureDetector(
+        onTap: () {
+          FocusScopeNode currentFocus = FocusScope.of(context);
+
+          if (!currentFocus.hasPrimaryFocus) {
+            currentFocus.unfocus();
+          }
+        },
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(left: 20, right: 10),
+                child: (_con.carts.length > 0 && _con.carts[0].product.market.pay_on_pickup) ? ListTile(
+                  contentPadding: EdgeInsets.symmetric(vertical: 0),
+                  leading: Icon(
+                    Icons.domain,
+                    color: Theme.of(context).hintColor,
+                  ),
+                  title: Text(
+                    S.of(context).pickup,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                  subtitle: Text(
+                    S.of(context).pickup_your_product_from_the_market,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: Theme.of(context).textTheme.caption,
+                  ),
+                ):
+                Container(),
+              ),
+              (_con.carts.length > 0 && _con.carts[0].product.market.pay_on_pickup) ? PickUpMethodItem(
+                  paymentMethod: _con.getPickUpMethod(),
+                  onPressed: (paymentMethod) {
+                    _con.togglePickUp();
+                  }
               ):
               Container(),
-            ),
-            (_con.carts.length > 0 && _con.carts[0].product.market.pay_on_pickup) ? PickUpMethodItem(
-                paymentMethod: _con.getPickUpMethod(),
-                onPressed: (paymentMethod) {
-                  _con.togglePickUp();
-                }
-            ):
-            Container(),
-            Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(top: 20, bottom: 10, left: 20, right: 10),
-                  child: ListTile(
-                    contentPadding: EdgeInsets.symmetric(vertical: 0),
-                    leading: Icon(
-                      Icons.map,
-                      color: Theme.of(context).hintColor,
-                    ),
-                    title: Text(
-                      S.of(context).delivery,
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      style: Theme.of(context).textTheme.headline4,
-                    ),
-                    subtitle: _con.carts.isNotEmpty && Helper.canDelivery(_con.carts[0].product.market, carts: _con.carts)
-                        ? Text(
-                      S.of(context).click_to_confirm_your_address_and_pay_or_long_press,
-                      maxLines: 3,
-                      overflow: TextOverflow.ellipsis,
-                      style: Theme.of(context).textTheme.caption,
-                    )
-                        : Text(
-                      S.of(context).deliveryMethodNotAllowed,
-                      maxLines: 3,
-                      overflow: TextOverflow.ellipsis,
-                      style: Theme.of(context).textTheme.caption,
-                    ),
-                  ),
-                ),
-                _con.carts.isNotEmpty && Helper.canDelivery(_con.carts[0].product.market, carts: _con.carts)
-                    ? Container(
-                    child: ListView(
-                        shrinkWrap: true,
-                        primary: false,
-                        children: <Widget>[
-                          DeliveryAddressesItemWidget(
-                            paymentMethod: _con.getDeliveryMethod(),
-                            address: _con.deliveryAddress,
-                            onPressed: (Address _address) {
-                              if (_con.deliveryAddress.id == null || _con.deliveryAddress.id == 'null') {
-                                DeliveryAddressDialog(
-                                  context: context,
-                                  address: _address,
-                                  onChanged: (Address _address) {
-                                    _con.addAddress(_address);
-                                  },
-                                );
-                              } else {
-                                _con.toggleDelivery();
-                              }
-                            },
-                            onLongPress: (Address _address) {
-                              DeliveryAddressDialog(
-                                context: context,
-                                address: _address,
-                                onChanged: (Address _address) {
-                                  _con.updateAddress(_address);
-                                },
-                              );
-                            },
-                          ),
-                          IconButton(
-                            onPressed: () {
-                              if (currentUser.value.apiToken != null) {
-                                var bottomSheetController = _con.scaffoldKey.currentState.showBottomSheet(
-                                      (context) => DeliveryAddressBottomSheetWidget(scaffoldKey: _con.scaffoldKey),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: new BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10)),
-                                  ),
-                                );
-                                bottomSheetController.closed.then((value) {
-                                  _con.addressUpdated(context);
-                                });
-                              }
-                            },
-                            icon: Icon(
-                              Icons.my_location,
-                              color: Theme.of(context).hintColor,
-                            ),
-                          )
-                        ]
-                    )
-                )
-                    : NotDeliverableAddressesItemWidget(),
-                Padding(
+              Column(
+                children: <Widget>[
+                  Padding(
                     padding: const EdgeInsets.only(top: 20, bottom: 10, left: 20, right: 10),
                     child: ListTile(
                       contentPadding: EdgeInsets.symmetric(vertical: 0),
                       leading: Icon(
-                        Icons.note_add,
+                        Icons.map,
                         color: Theme.of(context).hintColor,
                       ),
                       title: Text(
-                        S.of(context).add_note,
+                        S.of(context).delivery,
                         maxLines: 1,
                         overflow: TextOverflow.ellipsis,
                         style: Theme.of(context).textTheme.headline4,
                       ),
-                      subtitle: Text(
-                        S.of(context).add_note_helper,
-                        maxLines: 1,
+                      subtitle: _con.carts.isNotEmpty && Helper.canDelivery(_con.carts[0].product.market, carts: _con.carts)
+                          ? Text(
+                        S.of(context).click_to_confirm_your_address_and_pay_or_long_press,
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context).textTheme.caption,
+                      )
+                          : Text(
+                        S.of(context).deliveryMethodNotAllowed,
+                        maxLines: 3,
                         overflow: TextOverflow.ellipsis,
                         style: Theme.of(context).textTheme.caption,
                       ),
-                    )
-                ),
-                Padding(
-                    padding: const EdgeInsets.only(left: 20, right: 10),
-                    child:TextField(
-                      controller: _con.check_note,
-                      maxLines: 8,
-                      minLines: 4,
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        hintText: '',
-                        contentPadding:
-                        const EdgeInsets.only(left: 14.0, bottom: 8.0, top: 8.0),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black12),
-                          borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  _con.carts.isNotEmpty && Helper.canDelivery(_con.carts[0].product.market, carts: _con.carts)
+                      ? Container(
+                      child: ListView(
+                          shrinkWrap: true,
+                          primary: false,
+                          children: <Widget>[
+                            DeliveryAddressesItemWidget(
+                              paymentMethod: _con.getDeliveryMethod(),
+                              address: _con.deliveryAddress,
+                              onPressed: (Address _address) {
+                                if (_con.deliveryAddress.id == null || _con.deliveryAddress.id == 'null') {
+                                  DeliveryAddressDialog(
+                                    context: context,
+                                    address: _address,
+                                    onChanged: (Address _address) {
+                                      _con.addAddress(_address);
+                                    },
+                                  );
+                                } else {
+                                  _con.toggleDelivery();
+                                }
+                              },
+                              onLongPress: (Address _address) {
+                                DeliveryAddressDialog(
+                                  context: context,
+                                  address: _address,
+                                  onChanged: (Address _address) {
+                                    _con.updateAddress(_address);
+                                  },
+                                );
+                              },
+                            ),
+                            IconButton(
+                              onPressed: () {
+                                if (currentUser.value.apiToken != null) {
+                                  var bottomSheetController = _con.scaffoldKey.currentState.showBottomSheet(
+                                        (context) => DeliveryAddressBottomSheetWidget(scaffoldKey: _con.scaffoldKey),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: new BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10)),
+                                    ),
+                                  );
+                                  bottomSheetController.closed.then((value) {
+                                    _con.addressUpdated(context);
+                                  });
+                                }
+                              },
+                              icon: Icon(
+                                Icons.my_location,
+                                color: Theme.of(context).hintColor,
+                              ),
+                            )
+                          ]
+                      )
+                  )
+                      : NotDeliverableAddressesItemWidget(),
+                  Padding(
+                      padding: const EdgeInsets.only(top: 20, bottom: 10, left: 20, right: 10),
+                      child: ListTile(
+                        contentPadding: EdgeInsets.symmetric(vertical: 0),
+                        leading: Icon(
+                          Icons.note_add,
+                          color: Theme.of(context).hintColor,
                         ),
-                        enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black12),
-                          borderRadius: BorderRadius.circular(10),
+                        title: Text(
+                          S.of(context).add_note,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: Theme.of(context).textTheme.headline4,
                         ),
-                      ),
-                    )
-                )
-              ],
-            )
-          ],
+                        subtitle: Text(
+                          S.of(context).add_note_helper,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: Theme.of(context).textTheme.caption,
+                        ),
+                      )
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.only(left: 20, right: 10),
+                      child:TextField(
+                        controller: _con.check_note,
+                        maxLines: 8,
+                        minLines: 4,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          hintText: '',
+                          contentPadding:
+                          const EdgeInsets.only(left: 14.0, bottom: 8.0, top: 8.0),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black12),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black12),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      )
+                  )
+                ],
+              )
+            ],
+          ),
         ),
-      ),
+      )
     );
   }
 }
